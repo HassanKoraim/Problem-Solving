6c95fa5ad63bfcecca82625a9571072a91d7d2f6
Hassan Koraim <hassankoraim2@gmail.com>
2025-01-14 00:15:17 +0200

    First Commit

---
+++ ICPC Assiut University Community/Sheet #4 (Strings)/A. Create A New String/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace A._Create_A_New_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            Console.Write(input1.Length + " ");
+            Console.WriteLine(input2.Length);
+            Console.WriteLine(input1 + " " + input2);
+        }
+        
+    }
+}
