dc2a46ae11c2c4b0ce656b3302ed19415f0b261a
Hassan Koraim <hassankoraim2@gmail.com>
2025-01-15 23:47:51 +0200

    Say Hello problem

---
+++ ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/A. Say Hello With C#/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace A._Say_Hello_With_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string welcome = $"Hello, {name}";
+            Console.WriteLine(welcome);
+        }
+    }
+}
