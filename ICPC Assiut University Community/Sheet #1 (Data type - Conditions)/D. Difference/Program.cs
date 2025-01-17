c9268c6afc18701684bf1cc70d17539a7df049f0
Hassan Koraim <hassankoraim2@gmail.com>
2025-01-17 12:13:58 +0200

    Add 'D. Difference' Problem

---
+++ ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/D. Difference/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace D._Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            long A = long.Parse(inputs[0]);
+            long B = long.Parse(inputs[1]);
+            long C = long.Parse(inputs[2]);
+            long D = long.Parse(inputs[3]);
+            long x = (A*B) - (C*D);
+            Console.WriteLine($"Difference = {x}");
+        }
+    }
+}
