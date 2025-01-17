157accccab780426eb3db7ee5a76a3b4c6ed3cab
Hassan Koraim <hassankoraim2@gmail.com>
2025-01-17 12:28:49 +0200

    Add 'E. Area of a Circle' Problem

---
+++ ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/E. Area of a Circle/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace E._Area_of_a_Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            double Bye = 3.141592653;
+            double area = Bye * (r * r);
+            Console.WriteLine(area);
+        }
+    }
+}
