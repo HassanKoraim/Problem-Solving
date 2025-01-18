2dcf676fe9ced665fdd9fa9e94ec89329e538dae
Hassan Koraim <hassankoraim2@gmail.com>
2025-01-19 00:06:06 +0200

    Add 'F. Digits Summation' Problem

---
+++ ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/F. Digits Summation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace F._Digits_Summation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            long N = long.Parse(inputs[0]);
+            long M = long.Parse(inputs[1]);
+            long sum = (N % 10 ) + (M % 10);  
+            Console.WriteLine(sum);
+        }
+    }
+}
