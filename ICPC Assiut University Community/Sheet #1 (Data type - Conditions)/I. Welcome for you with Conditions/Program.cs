ed62f214fce42b105eb8997f40f61efca32ddd6a
Hassan Koraim <hassankoraim2@gmail.com>
2025-01-19 23:00:22 +0200

    Add 'I. Welcome for you with Conditions' Problem

---
+++ ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/I. Welcome for you with Conditions/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace I._Welcome_for_you_with_Conditions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            int A = int.Parse(inputs[0]);
+            int B = int.Parse(inputs[1]);
+            bool IsGreaterOrEqual(int x, int y) => x >= y;
+            if (IsGreaterOrEqual(A, B))
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+        }
+    }
+}
