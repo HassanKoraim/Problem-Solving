26658178483d61570e9cce818b0bc0e7a49bcbe3
Hassan Koraim <hassankoraim2@gmail.com>
2025-01-19 22:10:59 +0200

    Add 'G. Summation from 1 to N' Problem

---
+++ ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/G. Summation from 1 to N/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace G._Summation_from_1_to_N
+{
+    /// <summary>
+    /// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/G
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long input = long.Parse(Console.ReadLine());
+            long sum = (input * (input +1)) / 2; // Calculate  the summation of the numbers that is between 1 and input (inclusive)
+            Console.WriteLine(sum);
+        }
+    }
+}
