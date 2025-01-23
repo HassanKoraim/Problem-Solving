b8d3f0dd7bad62eea68d9ae86df120b5c8a7794c
Hassan Koraim <hassankoraim2@gmail.com>
2025-01-23 18:06:34 +0200

    Add 'H. Two numbers' Problem

---
+++ ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/H. Two numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace H._Two_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] vals = s.Split(" ");
+            double a = double.Parse(vals[0]);
+            double b = double.Parse(vals[1]);
+            double c = a / b;
+            // Console.WriteLine(c);
+            double floor = Math.Floor(c);
+            double ceil = Math.Ceiling(c);
+            double round = Math.Round(c, MidpointRounding.AwayFromZero);
+            Console.WriteLine(
+                $"floor {a} / {b} = {floor}" +
+                $"\r\nceil {a} / {b} = {ceil}" +
+                $"\r\nround {a} / {b} = {round}");
+
+        }
+    }
+}
