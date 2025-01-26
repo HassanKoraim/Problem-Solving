d6d8dcb4c35027319de94ebfd295fa05b9cbc015
Hassan Koraim <hassankoraim2@gmail.com>
2025-01-26 23:24:37 +0200

    Add 'J. Multiples' Problem

---
+++ ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/J. Multiples/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace J._Multiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(" ");
+            int A = int.Parse(inputs[0]);
+            int B = int.Parse(inputs[1]);
+            bool IsMultiple = A % B == 0 || B % A == 0;
+            if (IsMultiple)
+                Console.WriteLine("Multiples");
+            else
+                Console.WriteLine("No Multiples");
+        }
+    }
+}
