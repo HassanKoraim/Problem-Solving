635352b5ef1101ea4157d3d88f18c8ea7b95571c
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-11 20:29:45 +0200

    Add 'K. Max and Min' Problem

---
+++ ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/K. Max and Min/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace K._Max_and_Min
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+        
+
+            int min = int.Parse(inputs[0]);
+            int max = int.Parse(inputs[0]);
+            for(int i = 1; i < 3; i++)
+            {
+                if (int.Parse(inputs[i]) < min) min = int.Parse(inputs[i]);
+                if (int.Parse(inputs[i]) > max) max = int.Parse(inputs[i]);
+            }
+            Console.Write($"{min} "); Console.Write(max);
+            
+        }
+    }
+}
