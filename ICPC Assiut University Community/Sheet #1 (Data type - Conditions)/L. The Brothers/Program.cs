2b94651b740565862f111146006f1dd58b3bd312
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-16 22:17:13 +0200

    Add 'L. The Brothers' Problem

---
+++ ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/L. The Brothers/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace L._The_Brothers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstPersonName = Console.ReadLine().Split(' ');
+            string[] secondPersonName = Console.ReadLine().Split(' ');
+            string fatherNameP1 = firstPersonName[1];
+            string fatherNameP2 = secondPersonName[1];
+            string IsBrother = fatherNameP1 == fatherNameP2 ? "ARE Brothers": "NOT";
+            Console.WriteLine(IsBrother);
+        }
+    }
+}
