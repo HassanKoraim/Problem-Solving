2ca973ac1bf2d0418f43b7688bfdc5da8e7df32a
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-17 23:37:59 +0200

    M. Capital or Small or Digit

---
+++ ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/M. Capital or Small or Digit/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace M._Capital_or_Small_or_Digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char c =  Char.Parse(Console.ReadLine());
+            if(c >= 48 && c <= 57)
+            {
+                Console.WriteLine("IS DIGIT");
+            }
+            else
+            {
+                Console.WriteLine("ALPHA");
+                if(c >= 65 && c <= 90)
+                {
+                    Console.WriteLine("IS CAPITAL");
+                }
+                else
+                {
+                    Console.WriteLine("IS SMALL");
+                }
+            }
+        }
+    }
+}
