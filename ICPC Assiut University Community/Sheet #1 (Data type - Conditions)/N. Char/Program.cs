9c210565a82838d0fd225cd94e69763543af7af2
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-18 20:05:52 +0200

    Add 'N. Char' Problem

---
+++ ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/N. Char/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace N._Char
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           char c = char.Parse(Console.ReadLine());
+            if(c >= 65 && c <= 90)
+            {
+                int a = c + 32;
+                Console.WriteLine((char)a);
+            }
+            else
+            {
+                int a = c - 32;
+                Console.WriteLine((char)a);
+            }
+        }
+    }
+}
