ea3db4606db197bb031851308494f05815df9a61
Hassan Koraim <hassankoraim2@gmail.com>
2025-03-02 19:38:56 +0200

    O. Calculator

---
+++ ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/O. Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace O._Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputs = Console.ReadLine();
+            string[] inputs2 = inputs.Split('*', '+', '-', '/');
+            int a = int.Parse(inputs2[0]);
+            int b = int.Parse(inputs2[1]);
+
+            if (inputs.Contains('+')){
+                Console.WriteLine(a + b);
+            }else if (inputs.Contains("-"))
+            {
+                Console.WriteLine(a - b);
+            }else if (inputs.Contains("/"))
+            {
+                Console.WriteLine(a / b);
+            }else if (inputs.Contains('*'))
+            {
+                Console.WriteLine(a * b);
+            }
+
+        }
+    }
+}
